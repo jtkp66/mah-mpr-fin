14fa6679de32ed06009b0ca95e1f69f13f8a0cd1
Jamie Pham <jtkp66@gmail.com>
2020-02-13 02:04:10 -0800

    adding requirements file

--- survey_project/settings.py
+++ survey_project/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'survey_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "survdev",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
